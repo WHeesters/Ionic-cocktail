2f92d449805c1193e049d4057055818e4026fc66
Wesley <wesley.heesters@easyflex.nl>
2018-10-30 15:17:39 +0100

    Adds api calls

---
+++ src/app/cocktail-api.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+
+
+const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic';
+const idApiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CocktailApiService {
+
+    constructor(private http: HttpClient) { }
+
+    private handleError(error: HttpErrorResponse) {
+      if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it accordingly.
+        console.error('An error occurred:', error.error.message);
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
+      }
+      // return an observable with a user-facing error message
+      return throwError('Something bad happened; please try again later.');
+    }
+
+    private extractData(res: Response) {
+      let body = res;
+      return body || { };
+    }
+
+    getCocktail(): Observable<any> {
+      return this.http.get(apiUrl).pipe(
+        map(this.extractData),
+        catchError(this.handleError));
+    }
+
+    getCocktailById(id: string): Observable<any> {
+      const url = `${idApiUrl}/${id}`;
+      return this.http.get(url).pipe(
+        map(this.extractData),
+        catchError(this.handleError));
+    }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {LoadingController} from '@ionic/angular';
+import {CocktailApiService} from '../cocktail-api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+    cocktails: any;
+
+    constructor(public api: CocktailApiService, public loadingController: LoadingController) {
+        this.getCocktails();
+    }
+
+    async getCocktails() {
+      const loading = await this.loadingController.create({
+        message: 'Loading'
+          });
+          await loading.present();
+          await this.api.getCocktail()
+            .subscribe(res => {
+              console.log(res);
+              this.cocktails = res;
+              loading.dismiss();
+            }, err => {
+              console.log(err);
+              loading.dismiss();
+            });
+    }
 }
